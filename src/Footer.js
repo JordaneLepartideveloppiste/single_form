679dd22fb9217c4bb5d342ce912c501be171fba7
Jordane <jordanelepart@gmail.com>
2021-07-22 15:31:01 +0200

    Components in App it's better

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <span>Développé avec <strong>React</strong> at <strong>Le Reacteur</strong> by <em>Jordane</em>.</span>
+        </div>
+    );
+};
+
+export default Footer;
--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import StepTwo from "./StepTwo";
+import "./Form.scss"
 
 
 const Form = () => {
@@ -7,7 +8,11 @@ const Form = () => {
   const [name, setName] = useState("");    
   const [email, setEmail] = useState("");    
   const [pass, setPass] = useState("");    
-  const [confPass, setConfPass] = useState("");   
+  const [confPass, setConfPass] = useState(""); 
+  const [result, setResult] = useState(false);
+  const [borderColor, setBorderColor] = useState("black");
+  
+  
   
   const handleNameChange = (e) => {
     setName(e.target.value)
@@ -22,27 +27,73 @@ const Form = () => {
     setConfPass(e.target.value)
   }
 
+
+
   const handleSubmit = (e) => {
       e.preventDefault();
-      <StepTwo name={name} email={email} password={pass}/>
+      if (pass === confPass) {
+          setResult(true);
+        } else {
+        setBorderColor("red");
+        console.log("enculé");
+          }
+      
   }
 
-    return (
-        <div className="form">
-            <h2>Création de compte</h2>
-            <form onSubmit={handleSubmit}>
-                <label for="name">Votre petit nom, pseudo ou autre diminutif</label>
-                <input type="text" name="name" onChange= {handleNameChange} />
-                <label for="email">Une adresse email valide, c'est toujours mieux</label>
-                <input type="text" name="email" onChange= {handleEmailChange} />
-                <label for="pass">Votre mot de passe</label>
-                <input type="password" name="pass" onChange= {handlePasswordChange} />
-                <label for="confpass">Votre mot de passe une deuxième fois pour être sûr</label>
-                <input type="password" name="confpass" onChange= {handleConfPasswordChange} />
-                <input type="submit" value="Balance la sauce !" />
-            </form>
-            
-        </div>
+
+
+    return !result ? (
+      <div className="form">
+        <h2>Création de compte</h2>
+        <form onSubmit={handleSubmit}>
+          <label for="name">Votre petit nom, pseudo ou autre diminutif</label>
+          <input type="text" name="name" onChange={handleNameChange} />
+          <label for="email">
+            Une adresse email valide, c'est toujours mieux
+          </label>
+          <input
+            type="text"
+            name="email"
+            onChange={handleEmailChange}
+            style={{ border: "none" }}
+          />
+          <label for="pass">Votre mot de passe</label>
+          <div
+            className="div_pass"
+            style={{
+              borderWidth: 1,
+              borderStyle: "solid",
+              borderColor: { borderColor },
+            }}
+          >
+            <input
+              type="password"
+              name="pass"
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <label for="confpass">
+            Votre mot de passe une deuxième fois pour être sûr
+          </label>
+          <div
+            className="div_pass"
+            style={{
+              borderWidth: 1,
+              borderStyle: "solid",
+              borderColor: { borderColor },
+            }}
+          >
+            <input
+              type="password"
+              name="confpass"
+              onChange={handleConfPasswordChange}
+            />
+          </div>
+          <input type="submit" value="Balance la sauce !" />
+        </form>
+      </div>
+    ) : (
+      <StepTwo name={name} email={email} password={pass} />
     );
 };
 
